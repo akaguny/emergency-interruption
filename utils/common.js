6207166f324c80b2c25788530045f704a16e652b
Alexey Shcherbakov <Alexey.Shchbakov@billing.ru>
2017-02-15 12:29:44 +0300

    Добавил потерявшийся файл

---
+++ utils/common.js
@@ -0,0 +1,36 @@
+/**
+ * @file       common.js
+ * @package
+ * @fileoverview Неклассифицированные Javascript-методы
+ * @module utils/common
+ */
+
+var path = require('path'),
+    util = require('util');
+
+module.exports = {
+
+  /**
+   * Получение аргумента --'название аргумента' из командной строки
+   * @param {string} argument ( --suite | --testrail )
+   * @returns {string} Значение аргумента
+   */
+  getArgumentFromCommandLine : function (argument) {
+    var index = process.argv.indexOf(argument);
+
+    if (index !== -1) {
+      return (process.argv[index + 1]);
+    }
+  },
+
+  /**
+   * Содержит ли командная строка аргумент
+   * @param {string} argument Аргумент
+   * @returns {boolean} Признак: содержится ли аргумент в строке
+   * @example
+   * common.isArgumentPresentInCommandLine('--e')
+   */
+  isArgumentPresentInCommandLine : function (argument) {
+    return process.argv.indexOf(argument) !== -1;
+  }
+}
